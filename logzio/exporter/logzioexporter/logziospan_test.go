3de667d24a68c91b93c99e707721c1f17b1b604f
yotamloe <yotam12341@gmail.com>
2022-08-09 14:39:41 +0300

    Add `TestTransformToDbModelSpan` unit test

--- logzio/exporter/logzioexporter/logziospan_test.go
+++ logzio/exporter/logzioexporter/logziospan_test.go
@@ -47,3 +47,28 @@ func TestTransformToLogzioSpanBytes(tester *testing.T) {
 		tester.Error("error converting span to logzioSpan, JaegerTag is not found")
 	}
 }
+
+func TestTransformToDbModelSpan(tester *testing.T) {
+	inStr, err := ioutil.ReadFile("./testdata/span.json")
+	if err != nil {
+		tester.Fatalf(fmt.Sprintf("error opening sample span file %s", err.Error()))
+	}
+	var span model.Span
+	err = json.Unmarshal(inStr, &span)
+	if err != nil {
+		fmt.Println("json.Unmarshal")
+	}
+	newSpan, err := transformToLogzioSpanBytes(&span)
+	if err != nil {
+		tester.Fatalf(err.Error())
+	}
+	var testLogzioSpan LogzioSpan
+	err = json.Unmarshal(newSpan, &testLogzioSpan)
+	if err != nil {
+		tester.Fatalf(err.Error())
+	}
+	dbModelSpan := testLogzioSpan.transformToDbModelSpan()
+	if len(dbModelSpan.References) != 3 {
+		tester.Fatalf("Error converting logzio span to dbmodel span")
+	}
+}
